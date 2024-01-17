70fa0163a2bc6db38e6fbcb65a07342ea2e0f0ff
Risaldo10 <75126577+Risaldo10@users.noreply.github.com>
2024-01-17 14:06:45 +0000

    new_data_added

--- Hello.py
+++ Hello.py
@@ -18,19 +18,23 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import datetime
 
 LOGGER = get_logger(__name__)
 
 
 def run():
   st.set_page_config(page_title="HUI Stats",page_icon="👋",layout='wide')
-
   st.write("# Welcome to Stats! 👋")
   if 'hui_training' not in st.session_state:
-    hui_training = pd.read_csv('trainings_data.csv',delimiter=';')
-    hui_training['Number of Practices'] = hui_training.iloc[:,2:].sum(axis=1)
+    hui_training = pd.read_csv('training_data.csv',delimiter=';')
+    hui_training['Number of Practices'] = hui_training.iloc[:,3:].sum(axis=1)
+    hui_training.set_index('Player', inplace=True)
     hui_training.rename(columns={'Color':'Player Group'},inplace=True)
-    hui_training.index.name = 'Player'  
+    hui_training.replace({"Benjamin Alexander Luscombe-Schaumann": "Benjamin Luscombe-Schaumann"}, inplace=True)
+    hui_training.replace({"Joakim Hooge Grøndahl Berg": "Joakim Grøndahl Berg"}, inplace=True)
+    hui_training.replace({"Achilles Brangstrup Lund-Jacobsen": "Achilles Lund-Jacobsen"}, inplace=True)
+    
     st.session_state['hui_training'] = hui_training
 
   if 'hui_match' not in st.session_state:
@@ -44,21 +48,36 @@ def run():
   
   st.dataframe(st.session_state['hui_training'])
   col1, col2, col3, col4 = st.columns(4)
-  with col1:
-     st.date_input('Start Date:', )
-  
+    
   train_order = ['Name', 'Player Group','Number of Practices','Player']
-  custom_palette = {"Blue": "blue", "White": "green", "Red": "red"}
+  custom_palette = {"Blå": "blue", "Hvid": "Gray", "Rød": "red",'Grøn':'Green','Lilla':'Purple','Orange':'Orange'}
   
-  hui_temp = st.session_state['hui_training'][st.session_state['hui_training']['Number of Practices'] > 15]
+  
 
-  fig, ax = plt.subplots(figsize=(14, 8)) 
-  ax = sns.barplot(x='Name',  y="Number of Practices", data=hui_temp.sort_values(['Number of Practices'],ascending=False), hue='Player Group', palette=custom_palette, ax=ax)
-  ax.set_title('HUI 2017 - Træning (data fra 1 Januar 2022 til 13 November 2023)')
-  ax.tick_params(axis='x', rotation=70) 
-  
   # Calculate and add mean lines for each group
-  add_mean = st.checkbox('Add averages to graph')
+  with col1:
+    num_slicer =st.number_input('Show Last:',min_value=1,max_value=76,value=10 )
+    add_mean = st.checkbox('Add averages to graph')
+  with col2:
+    
+    transpose_graph = st.checkbox('Transpose "Axis" in Graph')
+
+  fig, ax = plt.subplots(figsize=(14, 8))
+  if transpose_graph: 
+    x_name='Name'
+    y_name="Number of Practices"
+  else:
+    x_name="Number of Practices"
+    y_name='Name'
+  
+  all_trainings = st.session_state['hui_training'].iloc[:,2:-1]
+
+  hui_temp = st.session_state['hui_training'][st.session_state['hui_training']['Number of Practices'] > num_slicer]
+  st.dataframe(all_trainings.iloc[:,-num_slicer:].sum(axis=1))
+  ax = sns.barplot(y=x_name,  x=y_name, data=hui_temp.iloc[num_slicer:].sort_values(['Number of Practices'],ascending=False), hue='Player Group', palette=custom_palette, ax=ax)
+  ax.set_title('HUI 2017 - Træning (data fra 14 August 2022 til 18 Januar 2024)')
+  ax.tick_params(axis='x', rotation=90) 
+  
   if add_mean:
     group_means = hui_temp.groupby('Player Group')['Number of Practices'].mean()
     for i, mean in group_means.items():
